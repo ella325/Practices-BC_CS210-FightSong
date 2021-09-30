eabf8d079fb7447c099a3d644c9b89adb5481ba6
ella325 <57724910+ella325@users.noreply.github.com>
2021-09-30 15:28:07 -0700

    Add files via upload

---
+++ FightSongs.java
@@ -0,0 +1,36 @@
+
+public class FightSongs {
+
+	public static void main(String[] args) {
+		goTeamGo();
+		System.out.println();
+		repeatM();
+		System.out.println();
+		repeatM();
+		System.out.println();
+		goTeamGo();
+		
+	}
+	
+	public static void goTeamGo() {
+		System.out.println("Go, team, go!");
+		System.out.println("You can do it.");
+		
+	}
+	
+	public static void youAreTheBest() {
+		System.out.println("You're the best,");
+		System.out.println("In the west.");
+	}
+	
+	public static void repeatM() {
+		goTeamGo();
+		youAreTheBest();
+		goTeamGo();
+		System.out.println();
+		
+	}
+	
+	
+
+}
